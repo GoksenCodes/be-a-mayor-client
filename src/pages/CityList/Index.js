088ea84b6c38e4e36c8137a0ce0538451db48165
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-07 11:42:02 +0200

    created simple thunk to dispatch cities to reducer

--- src/pages/CityList/Index.js
+++ src/pages/CityList/Index.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Jumbotron } from "react-bootstrap";
+import { fetchCityList } from '../../store/cityList/actions';
 
 export default function CityList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCityList());
+  }, [dispatch])
+
   return (
     <div className='city-list'>
       <Jumbotron>
-        <h1>Please choose your list of cities</h1>
+        <h1>Browse through our list of cities</h1>
       </Jumbotron>
     </div>
   )
