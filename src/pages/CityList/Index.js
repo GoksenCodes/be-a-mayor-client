fd7bb147f6854624b7ec151591b4a0d151cc4f15
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-07 11:18:57 +0200

    wired up simple CityList Page

---
+++ src/pages/CityList/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron } from "react-bootstrap";
+
+export default function CityList() {
+  return (
+    <div className='city-list'>
+      <Jumbotron>
+        <h1>Please choose your list of cities</h1>
+      </Jumbotron>
+    </div>
+  )
+}
