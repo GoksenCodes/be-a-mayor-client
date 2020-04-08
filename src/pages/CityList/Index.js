d9b59de0e7b820794e5eedde6bc685c7f722b922
JasonNelemans <jasonnelemans@hotmail.com>
2020-04-08 17:11:16 +0200

    minimal styling and reset functionality

--- src/pages/CityList/Index.js
+++ src/pages/CityList/Index.js
@@ -40,8 +40,6 @@ export default function CityList() {
     }
   }, [dispatch, minPopulation, maxPrice])
 
-
-
   const clickHandler = () => {
     dispatch(fetchCityByCondition(country, continent, population, price));
     setToggle('filter');
@@ -55,46 +53,48 @@ export default function CityList() {
         <h1>Browse through our list of cities</h1>
       </Jumbotron>
 
-      <div className='filter-buttons'>
-        <div className='country-filter'>
-          <label htmlFor="country-select">Sort by country:</label>
-          <select name="country" id="country-select" onChange={(e) => setCountry(e.target.value)}>
-            <option value="all">--Please choose an option--</option>
-            <option value="all">all</option>
-            {citiesList.map(city => {
-              return (
-                  <option value={city.country} key={city.id}>{city.country}</option>
-              )
-            })}
-          </select>
-        </div>
+      <form>
+        <div className='filter-buttons'>
+          <div className='country-filter'>
+            <label htmlFor="country-select">Sort by country:</label>
+            <select name="country" id="country-select" onChange={(e) => setCountry(e.target.value)}>
+              <option value="all">--Please choose an option--</option>
+              <option value="all">all</option>
+              {citiesList.map(city => {
+                return (
+                    <option value={city.country} key={city.id}>{city.country}</option>
+                )
+              })}
+            </select>
+          </div>
 
-        <div className='continent-filter'>
-          <label htmlFor="continent-select">Sort by continent:</label>
-          <select name="continent" id="continent-select" onChange={(e) => setContinent(e.target.value)}>
-            <option value="all">--Please choose an option--</option>
-            <option value="all">all</option>
-            {citiesList.map(city => {
-              return (
-                  <option value={city.continent} key={city.id}>{city.continent}</option>
-              )
-            })}
-          </select>
-        </div>
-        
-        <div className='population-filter'>
-          <label>Minimum value of population:</label>
-          <input type="range" value={population} min={minPopulation} max={maxPopulation} onChange={e => setPopulation(e.target.value)} />
-        </div>
+          <div className='continent-filter'>
+            <label htmlFor="continent-select">Sort by continent:</label>
+            <select name="continent" id="continent-select" onChange={(e) => setContinent(e.target.value)}>
+              <option value="all">--Please choose an option--</option>
+              <option value="all">all</option>
+              {citiesList.map(city => {
+                return (
+                    <option value={city.continent} key={city.id}>{city.continent}</option>
+                )
+              })}
+            </select>
+          </div>
+          
+          <div className='population-filter'>
+            <label>Minimum value of population:</label>
+            <input type="range" value={population} min={minPopulation} max={maxPopulation} onChange={e => setPopulation(e.target.value)} />
+          </div>
 
-        <div className='price-filter'>
-          <label>Maximum price:</label>
-          <input type="range" value={price} min={minPrice} max={maxPrice} onChange={e => setPrice(e.target.value)} />
-        </div>
+          <div className='price-filter'>
+            <label>Maximum price:</label>
+            <input type="range" value={price} min={minPrice} max={maxPrice} onChange={e => setPrice(e.target.value)} />
+          </div>
 
-        <Button variant="primary" onClick={clickHandler}>Set filters</Button>
-        <Button variant="primary" onClick={() => setToggle('list')}>Reset</Button>
-      </div>
+          <Button className='button' variant="primary" onClick={clickHandler}>Set filters</Button>
+          <Button className='button' type="reset" value="Reset" variant="primary" onClick={() => setToggle('list')}>Reset</Button>
+        </div>
+      </form>
 
       <div className='city-list'>
         {cities.map((city, num) => {
